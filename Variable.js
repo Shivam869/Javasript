843484537274a44499f06eb4a6cdfbfe2441efa4
Shivam869 <sanjivshivam23@gmail.com>
2024-08-03 22:06:33 +0530

    Add files via upload

---
+++ Variable.js
@@ -0,0 +1,5 @@
+var x,y,z
+x=1
+y=2
+z=x+y
+document.getElementById ("demo") .innerHTML="the value of z is ",z
---
+++ syntax & commenting.js
@@ -0,0 +1,2 @@
+document.getElementById ("h").innerHTML="Hello"
+document.getElementById ("p").innerHTML="I am Shivam"
